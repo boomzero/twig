ec3d0b432f38a47e99f785aa401a5dec2c9a8110
boomzero <thomas_rainbowfish@icloud.com>
2025-07-17 18:12:22 +0800

    init

---
+++ electron.vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [svelte(), tailwindcss()]
+  }
+})
